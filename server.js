c3d260d854f7ae857fea31f74ed007d0a94db06a
YaroslavS2022 <yaroslav.sakal2002@gmail.com>
2023-06-03 00:30:06 +0200

    API JS

---
+++ server.js
@@ -0,0 +1,173 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const xml2js = require('xml2js');
+const https = require('https');
+const parser = new xml2js.Parser();
+
+const port = 443;
+const app = express();
+const axios = require('axios');
+
+
+const options = {
+    key: fs.readFileSync('keys/private.key'),         // Path to your private key file
+    cert: fs.readFileSync('keys/certificate.crt'),        // Path to your certificate file
+  };
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.text({ type: 'application/xml' }));
+
+
+let data = [];
+let getMethods = ['securities', 'zberigs', 'deponenttypes',
+    'birgi', 'depoopers', 'depostats', 'depostate'];
+
+let postMethods = ['depoagrees', 'brokers', 'brokagrees', 
+    'fdaccnts', 'depodocs'];
+
+    
+// Necessary functions:
+function extractDataFromXML(fileName) {
+  const xmlData = fs.readFileSync(fileName, 'utf8');
+  // fileName += ".xml";
+  console.log(xmlData);
+  return xmlData;
+  // const parser = new xml2js.Parser();
+  // console.log("/n/n/nTESTESTEST/n/n");
+  // console.log(xmlData != null);
+  // let x = parser.parseString(xmlData, (err, result) => {
+  //   if (err) {
+  //     console.error('Error parsing XML:', err);
+  //     // Handle the error accordingly
+  //     return Error;
+  //   } else {
+  //     // Extract the desired data from the parsed XML
+  //     const extractedData = result.result;
+
+  //     console.log('Extracted data:', extractedData);
+  //     return extractedData;
+  //   }
+  // });
+  // return x;
+}
+/*TEST*/
+
+/*async function makePostRequest(url, data, contentType) {
+  try {
+    const response = await axios.post(url, data, {
+      headers: {
+        'Content-Type': contentType,
+      },
+    });
+
+    console.log('Response:', response.data);
+  } catch (error) {
+    console.error('POST Error:', error.message);
+  }
+}*/
+
+
+async function makePostRequest(url, data, contentType) {
+    try {
+      const response = await axios.post(url, data, {
+        headers: {
+          'Content-Type': contentType,
+        },
+        httpsAgent: new https.Agent({
+          rejectUnauthorized: false, // Ignore self-signed certificate errors
+        }),
+      });
+  
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('POST Error:', error.message);
+    }
+  }
+  
+  const url = 'https://localhost:443/data/json';
+  
+    /*TEST*/
+  
+  // GET endpoint
+  
+  app.get('/data', (req, res) => {
+    console.log(data);
+    res.send(data);
+    // console
+  });
+  
+  app.get('/XMLEntrySSL/do/depoapi/:parameter', (req, res) => {
+    // Process the GET request and send the response
+    // res.send('GET request received');
+    const parameter = req.params.parameter;
+    console.log(parameter + "\n\n");
+    if (getMethods.includes(parameter))
+      console.log("\n\nGET METHOD DETECTED\n\n");
+    else if (postMethods.includes(parameter))
+      console.log("\n\nPOST METHOD DETECTED\n\n");
+    else
+      console.log("\n\nSOMETHING WENT WRONG\n\n");
+    //const path = "./Depositorium/" + parameter;
+    console.log("");
+    const fs = require("fs")
+    try {
+      //const arrayOfFiles = fs.readdirSync("./Depositorium")
+      const name = "Depositorium/" + parameter + ".xml";
+      res.send(extractDataFromXML(name));
+      //console.log(arrayOfFiles)
+      console.log(extractDataFromXML(name));
+    } catch(e) {
+      console.log(e)
+    }
+    console.log("/n/n");
+    
+    console.log(data);
+  });
+  
+  // DELETE endpoint
+  app.delete('/data', (req, res) => {
+    // Process the DELETE request and send the response
+    res.send('DELETE request received');
+  });
+  
+  // PUT endpoint
+  app.put('/data', (req, res) => {
+    // Process the PUT request and send the response
+    res.send('PUT request received');
+  });
+  
+  
+  // POST endpoint for JSON data
+  app.post('/data/json', (req, res) => {
+    const jsonData = req.body;
+    console.log('Received JSON data:', jsonData);
+    data.push(jsonData);
+    makePostRequest(url, jsonData, 'application/json');
+    res.send('JSON data stored');
+  });
+  
+  // POST endpoint for XML data
+  app.post('/data/xml', (req, res) => {
+    const xmlData = req.body;
+    parser.parseString(xmlData, (err, result) => {
+      if (err) {
+        console.error('Error parsing XML data:', err);
+        res.status(400).send('Error parsing XML data');
+      } else {
+        console.log('Received XML data:', result);
+        data.push(result);
+        res.send('XML data stored');
+      }
+    });
+  });
+  
+  // Create an HTTP server
+  /*app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });*/
+  // Create HTTPS server
+  https.createServer(options, app).listen(port, () => {
+    console.log(`Server is running on https://localhost:${port}`);
+  });
